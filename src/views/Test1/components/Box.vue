629cac5a22f7d5338f5fe3282d0df7e714f3185d
KChrisZhang <zhanglufen@9fbank.com.cn>
2020-09-01 16:13:38 +0800

    add Test1/components/Box

---
+++ src/views/Test1/components/Box.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="box">{{value}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Box',
+  props: {
+    value: {
+      type: [String, Number]
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.box {
+  width: 100px;
+  background-color: gray;
+  border: 1px solid #ffffff;
+
+  padding: 10px;
+  margin: 30px auto;
+  color: #ffffff;
+  text-align: center;
+  cursor: pointer;
+
+  &:hover {
+    color: #333;
+    background-color: gainsboro;
+    border-color: gainsboro;
+  }
+}
+</style>
--- src/views/Test1/index.vue
+++ src/views/Test1/index.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="test">Test</div>
+  <div class="test">
+    <Box :value="boxValue" />
+  </div>
 </template>
 
 <script>
+import Box from './components/Box';
+
 export default {
-  name: 'Test'
+  name: 'Test',
+  components: {
+    Box
+  },
+  data() {
+    return {
+      boxValue: '124'
+    };
+  }
 };
 </script>
 
 <style lang="scss">
 .test {
+  background-color: #fff;
 }
 </style>
