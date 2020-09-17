bd9e4138d7212f510ce10e8a8fee4c337520ae66
KChrisZhang <zhanglufen@9fbank.com.cn>
2020-09-17 18:19:40 +0800

    add code

--- src/utils/learn_2/hello_2.js
+++ src/utils/learn_2/hello_2.js
@@ -1,7 +1,6 @@
 console.log('hello_2')
 
-console.log('\n')
-console.log('----------------------[demo1]')
+console.log('\n', '----------------------[demo1]')
 
 var foo = function(x) {
   x = x + 1
@@ -22,15 +21,13 @@ console.log(b1, 'b')
  * 第三章 原生函数
  */
 
-console.log('\n')
-console.log('----------------------[demo2]')
+console.log('\n', '----------------------[demo2]')
 
 var a2 = new Array(3)
 console.log(a2.length) // 2
 console.log(a2, 'a2') // [ <3 empty items> ]
 
-console.log('\n')
-console.log('----------------------[demo3]')
+console.log('\n', '----------------------[demo3]')
 
 var a3 = new Array(3)
 var b3 = [undefined, undefined, undefined]
@@ -41,8 +38,7 @@ console.log(a3, 'a3')
 console.log(b3, 'b3')
 console.log(c3, 'c3')
 
-console.log('\n')
-console.log('----------------------[demo4]')
+console.log('\n', '----------------------[demo4]')
 
 function fakeJoin(arr, connector) {
   var str = ''
@@ -60,8 +56,7 @@ var a4 = new Array(3)
 var b4 = fakeJoin(a4, '-') // --
 console.log(b4)
 
-console.log('\n')
-console.log('----------------------[demo5]')
+console.log('\n', '----------------------[demo5]')
 
 /**
  * 获取当前时间戳的两种方式
@@ -72,8 +67,7 @@ console.log(new Date()) // 不传递参数时，返回当前的日期和时间
 console.log(new Date().getTime())
 console.log(Date.now())
 
-console.log('\n')
-console.log('----------------------[demo6]')
+console.log('\n', '----------------------[demo6]')
 
 /**
  * 符号并非对象，而是一种简单标量基本类型
@@ -87,3 +81,96 @@ console.log(mysym) // Symbol(my own symbol)
 console.log(mysym.toString()) // 'Symbol(my own symbol)'
 console.log(typeof mysym) // 'symbol'
 console.log(Object.getOwnPropertySymbols(a6)) // [ Symbol(my own symbol) ]
+
+console.log('\n', '----------------------[demo7]')
+
+function isThisCool(vals, fn, rx) {
+  vals = vals || Array.prototype
+  fn = fn || Function.prototype
+  rx = rx || RegExp.prototype
+
+  console.log(vals, 'vals') // []
+  console.log(fn, 'fn') // [Function]
+  console.log(rx, 'rx') // RegExp {}
+
+  // return rx.test(vals.map(fn).join(''))
+}
+
+isThisCool()
+
+var a7 = {
+  val: [1, 2, 3],
+  toJSON: function() {
+    return this.val.slice(1)
+  },
+}
+
+var b7 = {
+  val: [1, 2, 3],
+  toJSON: function() {
+    return '[' + this.val.slice(1).join() + ']'
+  },
+}
+
+console.log(JSON.stringify(a7)) // [2,3]
+console.log(JSON.stringify(b7)) // "[2,3]"
+
+console.log('\n', '----------------------[demo8]')
+
+/**
+ * 可选参数 replacer，可以是数组或者函数
+ * 用来指定 对象序列化过程中 哪些属性应该被处理，哪些应该被排除，和 toJSON() 很像
+ */
+var a8 = {
+  b: 42,
+  c: '42',
+  d: [1, 2, 3],
+}
+
+/**
+ * 如果 replacer 是一个数组，那么它必须是一个 字符串数字
+ * 其中包含序列化要处理的对象的属性名称，除此之外其他的属性名称则被忽略
+ */
+console.log(JSON.stringify(a8, ['b', 'c'])) // {"b":42,"c":"42"}
+
+/**
+ * 如果 replacer 是一个函数，它会对对象本身调用一次
+ * 然后对对象中的每个属性各调用一次，每次传递两个参数，键和值。
+ */
+console.log(
+  JSON.stringify(a8, function(k, v) {
+    if (k !== 'c') return v
+  })
+)
+// {"b":42,"d":[1,2,3]}
+
+// JSON.stingify(...) 第三个可选参数，用来指定输出的缩进格式。
+console.log(JSON.stringify(a8, ['b', 'c'], '--'))
+// {
+//   --"b": 42,
+//   --"c": "42"
+//   }
+
+var date = new Date()
+console.log(date, 'date')
+console.log(+date, 'ts')
+
+console.log('\n', '----------------------[demo9]')
+
+/**
+ * 为啥打印18？
+ * 第一个错误：向 parseInt(...) 传递非字符串，此时JavaScript会将参数 强制类型转换为 它能够处理的字符串
+ * 第二个错误：基数19，在实际的JavaScript代码中不会用到基数19，它的有效数字字符范围是 0-9 和 a-i（区分大小写）
+ * parseInt(1/0, 19) 实际上是 parseInt('Infinity', 19)
+ * 第一个字符是 ‘I’，以19为基数时，数值为18；
+ * 第二个字符‘n’，不是一个有效的数字字符，解析到此位置，和‘42px’中的‘p’一样。
+ */
+console.log(parseInt(1 / 0, 19)) // 18
+
+var a9 = {
+  num: 21,
+  toString: function() {
+    return String(this.num * 2)
+  },
+}
+console.log(parseInt(a9))
