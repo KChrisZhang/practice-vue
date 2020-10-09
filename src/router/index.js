400ac21fbd5b72297d14a54b2be743fb243a6b04
KChris <kchris@MacBook-Pro-3.local>
2020-10-09 13:21:31 +0800

    refresh route

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,49 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "../views/Home";
 
-import Test from '../views/Test1'
-import Home from '../views/Home'
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: "/",
+      name: "home",
+      component: Home
     },
     {
-      path: '/test',
-      name: 'test',
-      component: Test,
+      path: "/test",
+      name: "test",
+      component: () => import("../views/Test1")
     },
     {
-      path: '/closet',
-      name: 'closet',
-      component: () => import('../views/Closet'),
+      path: "/closet",
+      name: "closet",
+      component: () => import("../views/Closet")
     },
     {
-      path: '/charts',
-      name: 'ChartIndex',
-      component: () => import('../views/Charts'),
-      redirect: { name: 'BarChart' },
+      path: "/charts",
+      name: "ChartIndex",
+      component: () => import("../views/Charts"),
+      redirect: { name: "BarChart" },
       children: [
         {
-          path: 'line',
-          name: 'LineChart',
-          component: () => import('../views/Charts/LineChart.vue'),
+          path: "line",
+          name: "LineChart",
+          component: () => import("../views/Charts/LineChart.vue")
         },
         {
-          path: 'bar',
-          name: 'BarChart',
-          component: () => import('../views/Charts/BarChart.vue'),
+          path: "bar",
+          name: "BarChart",
+          component: () => import("../views/Charts/BarChart.vue")
         },
         {
-          path: 'pie',
-          name: 'PieChart',
-          component: () => import('../views/Charts/PieChart.vue'),
-        },
-      ],
-    },
-  ],
-})
+          path: "pie",
+          name: "PieChart",
+          component: () => import("../views/Charts/PieChart.vue")
+        }
+      ]
+    }
+  ]
+});
